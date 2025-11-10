4ba89fae22a053656bab69284a254b3b8deb3802
Gyorgy0 <lukovics.gyuri@gmail.com>
2025-11-10 01:51:19 +0100

    Added Avalonia project files

---
+++ ImageProcessingFinal/ViewModels/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ImageProcessingFinal.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
+}
---
+++ ImageProcessingFinal/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ImageProcessingFinal.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
